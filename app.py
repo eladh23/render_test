25457fe417ed6735c7a868a0d710a16c469e59f1
eladh23 <eladelad121212@gmail.com>
2023-06-16 10:31:35 +0300

    some change

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 students = [{"id": 1, "name": "Dan"}, {"id": 2, "name": "Yochai"}]
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
+def index():
+    return render_template("students.html", students=students)
 
 if __name__ == "__main__":
     app.run()
